2b46736b8ad345293821a337561144341ce886ff
osparking <jbpark03@gmail.com>
2023-08-31 14:19:58 +0900

    configuration for context bean

---
+++ src/main/java/jb/ssh/ch3_e7/config/ProjectConfig.java
@@ -0,0 +1,10 @@
+package jb.ssh.ch3_e7.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("jb.ssh.ch3_e7.domain")
+public class ProjectConfig {
+
+}
